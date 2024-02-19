b3fc1af23842ab66021a2b2f675e57804668d83f
lbartosik <lbartosik@pepco.eu>
2024-02-19 09:50:15 +0100

    "With Ceneo test suite"

--- src/main/java/com/automatedtest/poc/model/pages/Offers.java
+++ src/main/java/com/automatedtest/poc/model/pages/Offers.java
@@ -16,6 +16,9 @@ public class Offers extends GeneralPage {
     private static final By acceptCookies=By.xpath("//*[contains(text(), 'Ok, zgadzam się')]");
     private static final By locateSearchBar=By.id("form-head-search-q");
     private static final By s23Offer=By.xpath("//*[contains(text(),'Samsung Galaxy S23 Ultra 5G SM-S918 12/512GB Czarny')]/parent::*");
+
+    private static final By numberOfOffersLocator=By.xpath("//a/span[contains(text(),'Oferty')]");
+    private static final By expandOffers=By.xpath("//span[@class='link link--accent show-remaining-offers__trigger js_remainingTrigger']");
     String rowsOfProducts = "//ul[@class='product-offers__list js_product-offers js_normal-offers js_async-offers-container-standard']/li[%s]";
 
     String rowsOfPrice="//ul[@class='product-offers__list js_product-offers js_normal-offers js_async-offers-container-standard']/li[%s]/div/div[1]";
@@ -29,14 +32,17 @@ public class Offers extends GeneralPage {
         setTextBySelector(product, locateSearchBar);
         scrollDownUp();
         clickBySelector(s23Offer);
-
+        clickBySelector(expandOffers);
     }
 
     public ArrayList<String> giveLowestPriceOffers() throws InterruptedException{
         ArrayList<String> priceList=new ArrayList<String>();
 //        ArrayList<String> priceList=new ArrayList<String>();
+
+        String cutOffers=getText(numberOfOffersLocator).replace("Oferty", "");
+        int offerListLength=Integer.parseInt(cutOffers.replaceAll("[()\\s]", ""));
         scrollDownUp();
-        for(int i=1;i<17;i++){
+        for(int i=1;i<offerListLength;i++){
 
 //            String xpathExpressionOffers = String.format(rowsOfProducts, i);
 
